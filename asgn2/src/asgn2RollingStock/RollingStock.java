aaaefe7ff2c989e401e7c464a2192af2d0cbd3f0
rablindaniel <rablin.daniel@gmail.com>
2013-05-23 19:47:09 +1000

    RollingStock: Written classes, passes tests

--- asgn2/src/asgn2RollingStock/RollingStock.java
+++ asgn2/src/asgn2RollingStock/RollingStock.java
@@ -1,5 +1,42 @@
 package asgn2RollingStock;
 
-public class RollingStock {
+import asgn2Exceptions.TrainException;
 
+public abstract class RollingStock extends Object
+{
+	private Integer grossweight;
+	
+	/**
+	 * Constructor - Initialises a new RollingStock.
+	 * @param grossWeight - A positive integer
+	 * @throws TrainException Exception thrown when grossWeight is below zero.
+	 * 
+	 * @author Daniel Rablin - n8038848
+	 */
+	public RollingStock(Integer grossWeight) throws TrainException
+	{
+		if(grossWeight < 0)
+		{
+			throw new TrainException("Gross weight cannot be less than zero.");
+		}
+		grossweight = grossWeight;
+	}
+	
+	/**
+	 * @return Integer Gross weight of carriage.
+	 * @author Daniel Rablin - n8038848
+	 */
+	public Integer getGrossWeight()
+	{
+		return grossweight;
+	}
+	
+	/**
+	 * @return String Human-readable description of carraige.
+	 * @author Daniel Rablin - n8038848
+	 */
+	@Override public String toString()
+	{
+		return "Type: RollingStock.\nGross Weight: " + grossweight.toString();
+	}
 }
