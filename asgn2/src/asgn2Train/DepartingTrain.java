15ec85dee89af3b32460a54c0187d8d2dfee3090
Bryce Kiefer <bryce1992@live.com.au>
2013-05-23 21:36:05 +1000

    Completed DepartingTrain class

--- asgn2/src/asgn2Train/DepartingTrain.java
+++ asgn2/src/asgn2Train/DepartingTrain.java
@@ -2,6 +2,7 @@ package asgn2Train;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 import asgn2Exceptions.TrainException;
 import asgn2RollingStock.FreightCar;
@@ -15,7 +16,6 @@ public class DepartingTrain
 	private LinkedList<RollingStock> locomotives;
 	private LinkedList<RollingStock> passengerCars;
 	private LinkedList<RollingStock> freightCars;
-	private int locomotiveCount = 0, passengerCarCount = 0, freightCarCount = 0, passengers = 0, passengersMax = 0;
 	private Iterator<RollingStock> currentCarriageIterator;
 	
 	public DepartingTrain()
@@ -28,6 +28,18 @@ public class DepartingTrain
 	
 	public void addCarriage(RollingStock newCarriage) throws TrainException
 	{
+		//Get counts for each kind of carriage
+		Integer locomotiveCount = 0, passengerCarCount = 0, freightCarCount = 0;
+		Iterator<RollingStock> iterator = carriages.iterator();
+		while (iterator.hasNext())
+		{
+			RollingStock carriage = iterator.next();
+			if (carriage instanceof Locomotive)			locomotiveCount++;
+			if (carriage instanceof PassengerCar)		passengerCarCount++;
+			if (carriage instanceof FreightCar)			freightCarCount++;
+		}
+		
+		//Test to see if the new carriage can be added
 		if (newCarriage instanceof Locomotive)
 		{
 			if (!carriages.isEmpty()) throw new TrainException("Unable to add locomotive; one or more carriages have already been added to this train.");
@@ -39,7 +51,9 @@ public class DepartingTrain
 			if (locomotiveCount == 0) throw new TrainException("Unable to add passenger car; a locomotive hasn't been added to the train yet.");
 			if (freightCarCount > 0) throw new TrainException("Unable to add passenger car; one or more freight cars have already been added to the train.");
 			passengerCarCount++;
-			passengerCars.add((PassengerCar)newCarriage);
+			PassengerCar p = (PassengerCar)newCarriage;
+			
+			passengerCars.add(p);
 		}
 		else if (newCarriage instanceof FreightCar)
 		{
@@ -49,20 +63,20 @@ public class DepartingTrain
 		}
 		carriages.add(newCarriage);
 	}
-	public int board(int newPassengers) throws TrainException
+	public Integer board(Integer newPassengers) throws TrainException
 	{
 		if (newPassengers < 0) throw new TrainException("Unable to board passengers; number of new passengers is negative");
-		if (passengers >= passengersMax) return newPassengers;
+		Iterator<RollingStock> iterator = carriages.iterator();
+		while (iterator.hasNext() && newPassengers.intValue() > 0)
 		{
-			int allPassengers = passengers + newPassengers;
-			if (allPassengers >= passengersMax)
+			RollingStock carriage = iterator.next();
+			if (carriage instanceof PassengerCar)
 			{
-				passengers = passengersMax;
-				return allPassengers - passengersMax;
+				PassengerCar p = (PassengerCar)carriage;
+				newPassengers = p.board(newPassengers);
 			}
 		}
-		passengers += newPassengers;
-		return 0;
+		return newPassengers;
 	}
 	public RollingStock firstCarriage()
 	{
@@ -81,5 +95,75 @@ public class DepartingTrain
 		}
 		return null;
 	}
-
+	public Integer numberOfSeats()
+	{
+		Iterator<RollingStock> iterator = carriages.iterator();
+		Integer count = new Integer(0);
+		while (iterator.hasNext())
+		{
+			RollingStock carriage = iterator.next();
+			if (carriage instanceof PassengerCar)
+			{
+				PassengerCar p = (PassengerCar)carriage;
+				count += p.numberOfSeats();
+			}
+		}
+		return count;
+	}
+	public Integer numberOnBoard()
+	{
+		Iterator<RollingStock> iterator = carriages.iterator();
+		Integer count = new Integer(0);
+		while (iterator.hasNext())
+		{
+			RollingStock carriage = iterator.next();
+			if (carriage instanceof PassengerCar)
+			{
+				PassengerCar p = (PassengerCar)carriage;
+				count += p.numberOnBoard();
+			}
+		}
+		return count;
+	}
+	public void RemoveCarriage() throws TrainException
+	{
+		if (numberOnBoard() > 0) throw new TrainException("Unable tp remove carriage; there are passengers on board this train");
+		try
+		{
+			carriages.removeLast();
+		}
+		catch (NoSuchElementException e)
+		{
+			throw new TrainException("Unable tp remove carriage; there are no carriages to remove");
+		}
+	}
+	public String toString()
+	{
+		String s = new String();
+		Iterator<RollingStock> iterator = carriages.iterator();
+		if (iterator.hasNext()) s += iterator.next().toString();
+		else return s;
+		while (iterator.hasNext())
+		{
+			s += "-" + iterator.next().toString();
+		}
+		return s;
+	}
+	public boolean canMove()
+	{
+		Iterator<RollingStock> iterator = carriages.iterator();
+		Integer weight = 0, power = 0;
+		while (iterator.hasNext())
+		{
+			RollingStock carriage = iterator.next();
+			if (carriage instanceof Locomotive)
+			{
+				Locomotive l = (Locomotive)carriage;
+				power += l.power();
+			}
+			weight += carriage.getGrossWeight();
+		}
+		if (power >= weight) return true;
+		return false;
+	}
 }
